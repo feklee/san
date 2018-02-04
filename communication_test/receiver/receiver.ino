e028d8bde2517637eb55f22e8e76a0567a7b0f22
Felix E. Klee <felix.klee@inka.de>
2018-02-04 20:54:03 +0100

    Test proper serial communication

--- communication_test/receiver/receiver.ino
+++ communication_test/receiver/receiver.ino
@@ -1,14 +1,26 @@
-#include "DigiKeyboard.h"
+// Tested with Digispark @16.5/16/8 Mhz
 
-int sensorValue = 0;
+#include <SoftSerial.h>
+#include <TinyPinChange.h>
+
+const uint8_t ledPin = 1;
+const uint8_t txRxPin = 0;
+
+SoftSerial mySerial(txRxPin, txRxPin); // RX, TX
 
 void setup() {
-  pinMode(0, INPUT);
-  Serial.begin(9600);
+  mySerial.begin(4800);
+  mySerial.rxMode();
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
 }
 
 void loop() {
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.println(digitalRead(0));
-  DigiKeyboard.delay(1000);
+  if (mySerial.available()) {
+    mySerial.read();
+    digitalWrite(ledPin, HIGH);
+    delay(100);
+    digitalWrite(ledPin, LOW);
+  }
+  delay(300);
 }
--- communication_test/sender/sender.ino
+++ communication_test/sender/sender.ino
@@ -1,10 +1,24 @@
+// Tested with Digispark @16.5/16/8 Mhz
+
+#include <SoftSerial.h>
+#include <TinyPinChange.h>
+
+const uint8_t ledPin = 1;
+const uint8_t txRxPin = 0;
+
+SoftSerial mySerial(txRxPin, txRxPin); // RX, TX
+
 void setup() {
-  pinMode(0, OUTPUT);
+  mySerial.begin(4800);
+  mySerial.txMode();
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(0, HIGH);
-  delay(5000);
-  digitalWrite(0, LOW);
-  delay(5000);
+  mySerial.write("Hi!");
+  digitalWrite(ledPin, HIGH);
+  delay(100);
+  digitalWrite(ledPin, LOW);
+  delay(100);
+  digitalWrite(ledPin, HIGH);
 }

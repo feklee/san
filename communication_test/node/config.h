b518a1c8ee8fd7cc7506ba911e544bf860e33047
Felix E. Klee <felix.klee@inka.de>
2018-03-16 12:16:14 +0100

    Increase number of ports to four

--- communication_test/node/config.h
+++ communication_test/node/config.h
@@ -6,8 +6,8 @@
 #define DEBUG
 
 const uint8_t ledPin = 13;
-const uint8_t portsCount = 2;
-const uint8_t portPins[portsCount] = {3, 2};
+const uint8_t portsCount = 4;
+const uint8_t portPins[portsCount] = {2, 3, 4, 5};
 const unsigned long graceTime = 100; // time for other node to switch to receive
 const unsigned long timeSlotDuration = 500; // ms
 const uint8_t pairBufferSize = 10;

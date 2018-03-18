7925af7befc063a8ead63d39bf1145fc84ccfc89
Felix E. Klee <felix.klee@inka.de>
2018-03-18 19:16:40 +0100

    Reintroduce scrollig in log

--- software/webapp/public/scripts/log.js
+++ software/webapp/public/scripts/log.js
@@ -15,6 +15,8 @@ var append = function (type, text) {
     logEl.appendChild(liEl).setAttribute("class", type);
     var preEl = document.createElement("pre");
     liEl.appendChild(preEl).textContent = prettyPrinted(text);
+    var parentEl = logEl.parentElement;
+    parentEl.scrollTop = parentEl.scrollHeight;
 };
 
 export default {

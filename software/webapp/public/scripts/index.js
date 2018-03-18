fcab7374d7b626fdd932306b8c88d9b520dc8955
Felix E. Klee <felix.klee@inka.de>
2018-03-18 20:47:24 +0100

    Add three.js example

--- software/webapp/public/scripts/index.js
+++ software/webapp/public/scripts/index.js
@@ -4,6 +4,7 @@ import log from "./log.js";
 
 var hostname = window.location.hostname;
 var client = new window.WebSocket("ws://" + hostname + ":8080/");
+const asideWidth = 300;
 
 client.onerror = function () {
     log.append("error", "WebSocket error");
@@ -29,3 +30,58 @@ client.onmessage = function (e) {
 
     log.append(data.type, data.text);
 };
+
+var camera, scene, renderer;
+var geometry, material, mesh;
+
+var visualizationEl = document.querySelector("div.visualization");
+
+function init() {
+    var width = window.innerWidth - asideWidth;
+    var height = window.innerHeight;
+
+    console.log(window.innerWidth);
+    console.log(width);
+
+	camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
+	camera.position.z = 1;
+
+	scene = new THREE.Scene();
+
+	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new THREE.MeshNormalMaterial();
+
+	mesh = new THREE.Mesh( geometry, material );
+	scene.add( mesh );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize(width, height);
+	visualizationEl.appendChild( renderer.domElement );
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.02;
+
+	renderer.render(scene, camera);
+}
+
+function onWindowResize() {
+    var width = window.innerWidth - asideWidth;
+    var height = window.innerHeight;
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(width, height);
+}
+
+window.addEventListener("resize", onWindowResize, false);
+
+document.addEventListener("DOMContentLoaded", function() {
+    var asideEl = document.querySelector("aside");
+    asideEl.style.width = asideWidth + "px";
+    init();
+    animate();
+});

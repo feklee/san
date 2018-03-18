3d1be058308b2f041e52b223689070ff6113748a
Felix E. Klee <felix.klee@inka.de>
2018-03-18 10:08:44 +0100

    Switch to ES6 modules

--- communication_test/app/public/scripts/index.js
+++ communication_test/app/public/scripts/index.js
@@ -1,32 +1,27 @@
-/*jslint browser: true, es6: true, maxlen: 80 */
+/*jslint browser: true, maxlen: 80 */
 
-/*global define, window, d3 */
+import log from "./log.js";
 
-define(["log"], function (log) {
-    "use strict";
+var hostname = window.location.hostname;
+var client = new window.WebSocket("ws://" + hostname + ":8080/");
 
-    var hostname = window.location.hostname;
-    var client = new window.WebSocket("ws://" + hostname + ":8080/");
-    var modeName = "";
+client.onerror = function () {
+    log.appendError("WebSocket error");
+};
 
-    client.onerror = function () {
-        log.appendError("WebSocket error");
-    };
+client.onopen = function () {
+    log.appendInfo("WebSocket opened");
+};
 
-    client.onopen = function () {
-        log.appendInfo("WebSocket opened");
-    };
+client.onclose = function () {
+    log.appendWarn("WebSocket closed");
+};
 
-    client.onclose = function () {
-        log.appendWarn("WebSocket closed");
-    };
-
-    client.onmessage = function (e) {
-        if (typeof e.data !== "string") {
-            return;
-        }
-        var json = e.data;
-        var data = JSON.parse(json);
-        log.appendError(data.value);
-    };
-});
+client.onmessage = function (e) {
+    if (typeof e.data !== "string") {
+        return;
+    }
+    var json = e.data;
+    var data = JSON.parse(json);
+    log.appendError(data.value);
+};
--- communication_test/app/public/scripts/log.js
+++ communication_test/app/public/scripts/log.js
@@ -1,40 +1,38 @@
-/*jslint browser: true, es6: true, maxlen: 80 */
+/*jslint browser: true, maxlen: 80 */
 
-/*global define, window, d3 */
+/*global d3 */
 
-define(function () {
-    "use strict";
+var logEl = d3.select("ul.log");
 
-    var logEl = d3.select("ul.log");
+var prettyPrinted = function (text) {
+    try {
+        return JSON.stringify(JSON.parse(text), null, 2);
+    } catch (ignore) {
+        return text;
+    }
+};
 
-    var prettyPrinted = function (text) {
-        try {
-            return JSON.stringify(JSON.parse(text), null, 2);
-        } catch (ignore) {
-            return text;
-        }
-    };
+var append = function (type, text) {
+    logEl.append("li").attr("class", type).append("pre").text(
+        prettyPrinted(text)
+    );
+    var parentNode = logEl.node().parentNode;
+    parentNode.scrollTop = parentNode.scrollHeight;
+};
 
-    var append = function (type, text) {
-        logEl.append("li").attr("class", type).append("pre").text(
-            prettyPrinted(text)
-        );
-        var parentNode = logEl.node().parentNode;
-        parentNode.scrollTop = parentNode.scrollHeight;
-    };
+var log = {
+    appendInput: function (text) {
+        append("input", text);
+    },
+    appendError(text) {
+        append("error", text);
+    },
+    appendWarn(text) {
+        append("warn", text);
+    },
+    appendInfo(text) {
+        append("info", text);
+    }
+};
 
-    return {
-        appendInput(text) {
-            append("input", text);
-        },
-        appendError(text) {
-            append("error", text);
-        },
-        appendWarn(text) {
-            append("warn", text);
-        },
-        appendInfo(text) {
-            append("info", text);
-        }
-    };
-});
+export default log;

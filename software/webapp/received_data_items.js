ca31aacc53f88d7cfa228f1b38ee12e6aaa6f8a6
Felix E. Klee <felix.klee@inka.de>
2018-03-25 21:34:51 +0200

    Remove debug data

--- software/webapp/received_data_items.js
+++ software/webapp/received_data_items.js
@@ -10,12 +10,6 @@ var add = function (data) {
 };
 
 var send = function () {
-//    ["*1B3", "*1_0", "*1B4", "B1F1", "F4C3", "B1_0", "F4C3", "B1F1", "F4C3", "F3A4", "C4E3"]. // todo: for debugging
-    ["*1B4", "B1F1", "F4C3", "F3A4", "C4E3"]. // todo: for debugging
-        forEach(function (data) {
-        items.push(data);
-    });
-
     items.forEach(function (data) {
         var message = {type: "data", text: data};
         browser.send(message);

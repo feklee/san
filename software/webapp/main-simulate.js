0f6296ab7ba130f845441b5e0127f952a6dc2abd
Felix E. Klee <felix.klee@inka.de>
2018-09-05 19:55:30 +0200

    Adapt simulation to new firmware on nodes

--- software/webapp/main-simulate.js
+++ software/webapp/main-simulate.js
@@ -4,13 +4,37 @@
 
 var startWebServer = require("./start-web-server");
 var webSocket = require("./web-socket");
-var receivedDataItems = require("./received-data-items");
 var cli = require("./cli");
+var pairs = new Set();
+var sendInterval = 150; // ms (should be ~ typical announce interval
+                        // in nodes)
 
-startWebServer(function () {
-    cli.enableInput(function (data) {
-        var message = {type: "data", text: data};
+function sendSet() {
+    pairs.forEach(function (pair) {
+        var message = {type: "data", text: pair};
         webSocket.send(message);
-        receivedDataItems.push(data);
+    });
+}
+
+setInterval(sendSet, sendInterval);
+
+console.log("Add pair, by example: +*1A3");
+console.log("Remove pair, by example: -A2B1");
+startWebServer(function () {
+    cli.enableInput(function (command) {
+        if (!(/^[+\-]([a-zA-Z*][1-4][a-zA-Z][1-4])$/).test(command)) {
+            console.error("Malformed command");
+            return;
+        }
+        var pair = command.substr(1);
+        switch (command.charAt(0)) {
+        case "+":
+            console.log("Adding ", pair);
+            pairs.add(pair);
+            break;
+        case "-":
+            pairs.delete(pair);
+            break;
+        }
     });
 });

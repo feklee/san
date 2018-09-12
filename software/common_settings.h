1c3385439fec90d5da94c9caaaf715e62aee66a5
Felix E. Klee <felix.klee@inka.de>
2018-09-12 15:25:59 +0200

    Add const type necessary for C++

--- software/common_settings.h
+++ software/common_settings.h
@@ -1,8 +1,9 @@
 // Common settings, used by nodes and the web app
 
 // How often the entire graph is updated / sent:
-graphUpdateInterval = 150; // ms
+const uint32_t graphUpdateInterval = 150; // ms
 
 // How long to wait for a connection to be removed after it has been
 // without sign of life:
-connectionExpiryDuration = 2.5 * graphUpdateInterval; // ms
+const uint32_t connectionExpiryDuration =
+  2.5 * graphUpdateInterval; // ms

3cd3e5f0a8a91cda346dbbc630d0d74355e30601
Felix E. Klee <felix.klee@inka.de>
2018-09-15 15:22:50 +0200

    Define colors for 22 nodes

--- software/nodes/eepromWriter/colors.h
+++ software/nodes/eepromWriter/colors.h
@@ -1,5 +1,9 @@
 const byte black[] = {0, 0, 0};
 const byte gray[] = {0x80, 0x80, 0x80};
-const byte orange[] = {0xff, 0xa5, 0};
-const byte green[] = {0, 0xff, 0};
+const byte white[] = {0xff, 0xff, 0xff};
+const byte red[] = {0xff, 0, 0};
+const byte yellow[] = {0xff, 0xff, 0};
+const byte lime[] = {0, 0xff, 0};
+const byte aqua[] = {0, 0xff, 0xff};
 const byte blue[] = {0, 0, 0xff};
+const byte fuchsia[] = {0xff, 0, 0xff};

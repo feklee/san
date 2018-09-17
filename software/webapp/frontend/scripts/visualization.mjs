d562a98140cfa30d9eff8804d9e3ee83aa1d7c12
Felix E. Klee <felix.klee@inka.de>
2018-09-17 15:29:00 +0200

    Increase viewing distance by resetting to default

--- software/webapp/frontend/scripts/visualization.mjs
+++ software/webapp/frontend/scripts/visualization.mjs
@@ -168,7 +168,7 @@ animate = function () {
 };
 
 var init = function () {
-    camera = new PerspectiveCamera(50, 1, 0.01, 10);
+    camera = new PerspectiveCamera(50);
 
     camera.position.z = 3;
 

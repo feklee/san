c0736037581d0de8839e71d56c51c073aabc4d88
Felix E. Klee <felix.klee@inka.de>
2018-11-29 12:21:32 +0100

    Add calibration tool for accelerometer

---
+++ nodes/AngleTableBuilder/AngleTableBuilder.ino
@@ -0,0 +1,99 @@
+const uint8_t accelerometerPin = A0;
+
+uint16_t minReadout, maxReadout;
+const unsigned long printInterval = 500; // ms
+
+void setup() {
+  analogReference(EXTERNAL);
+  Serial.begin(9600);
+  reset();
+}
+
+float acceleration(const uint16_t readout) { // g
+  uint16_t readoutPerG = (maxReadout - minReadout) / 2;
+  uint16_t zeroGReadout = minReadout + readoutPerG;
+  return (float(readout) - zeroGReadout) / readoutPerG;
+}
+
+float angle(uint16_t readout) { // deg
+  float acc = acceleration(readout);
+  float clippedAcc = min(1, max(-1, acc)); // [-1, 1]
+  return acos(clippedAcc) * 360 / 2 / PI;
+}
+
+// [0, 180] -> [1, 127] (0 excluded, because that means: no angle)
+uint16_t encodedAngle(uint16_t readout) {
+  float a = angle(readout);
+  return round(a * 126 / 180) % 127 + 1;
+}
+
+void printAngleTable() {
+  uint16_t readout = 0;
+  for (uint8_t row = 0; row < 128; row ++) {
+    for (uint8_t column = 0; column < 8; column ++) {
+      Serial.print(encodedAngle(readout));
+      Serial.print(readout < 1023 ? "," : "");
+      Serial.print(column < 7 ? " " : "");
+      readout ++;
+    }
+    Serial.println();
+  }
+}
+
+void printStatus(uint16_t lastReadout) {
+  char s[5];
+  Serial.print("last readout = ");
+  sprintf(s, "%4d", lastReadout);
+  Serial.print(s);
+  Serial.print(", min. readout = ");
+  sprintf(s, "%4d", minReadout);
+  Serial.print(s);
+  Serial.print(", max. readout = ");
+  sprintf(s, "%4d", maxReadout);
+  Serial.print(s);
+  Serial.println(" (send: `t` to print table, `r` to reset)");
+}
+
+uint16_t measure() {
+  uint16_t readout = analogRead(accelerometerPin);
+  minReadout = min(readout, minReadout);
+  maxReadout = max(readout, maxReadout);
+  return readout;
+}
+
+void reset() {
+  minReadout = 0xffff;
+  maxReadout = 0;
+}
+
+void iterate() {
+  static unsigned long scheduledPrintTime = millis();
+  uint16_t readout = measure();
+  unsigned long currentTime = millis();
+  if (currentTime >= scheduledPrintTime) {
+    printStatus(readout);
+    scheduledPrintTime = currentTime + printInterval;
+  }
+}
+
+void loop() {
+  static bool finished = false;
+
+  while (Serial.available() > 0) {
+    char c = Serial.read();
+    switch (c) {
+    case 't':
+      printAngleTable();
+      finished = true;
+      break;
+    case 'r':
+      reset();
+      finished = false;
+      break;
+    }
+  }
+
+  if (!finished) {
+    iterate();
+  }
+}

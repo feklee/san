dc2a1b57e013286107a56e1b9213e5f1f0fd0410
Felix E. Klee <felix.klee@inka.de>
2019-01-13 15:51:12 +0100

    Add get-fitness API (without coordinates yet)

--- webapp/main.js
+++ webapp/main.js
@@ -8,4 +8,5 @@ program
     .command("list", "List serial ports")
     .command("connect <port>", "Connect to root node")
     .command("simulate", "Simulate connection")
+    .command("api", "Provide API")
     .parse(process.argv);

15b9f5806b7b06a09b19bb54a5c619a291e949ba
Felix E. Klee <felix.klee@inka.de>
2019-06-05 16:56:19 +0200

    Add visualization for oscillator

--- webapp/frontend/scripts/node-ui.mjs
+++ webapp/frontend/scripts/node-ui.mjs
@@ -4,6 +4,66 @@ var hostname = window.location.hostname;
 var client = new window.WebSocket("ws://" + hostname + ":8080/");
 var nodeId = window.location.pathname.substr(1, 1);
 var baseFreqSliderEl = document.getElementById("base-freq-slider");
+var audioCtx = new window.AudioContext();
+
+const canvasEl = document.querySelector("canvas");
+const canvasCtx = canvasEl.getContext("2d");
+var oscillatorGain = audioCtx.createGain();
+oscillatorGain.connect(audioCtx.destination);
+var oscillatorOffset = audioCtx.createConstantSource();
+oscillatorOffset.connect(oscillatorGain);
+oscillatorOffset.start();
+var oscillator = audioCtx.createOscillator();
+oscillator.start();
+oscillator.connect(oscillatorGain);
+oscillatorGain.connect(audioCtx.destination);
+
+var showButtonEl = document.querySelector("button.show");
+showButtonEl.onclick = function () {
+    audioCtx.resume();
+    selectedOscillatorFreq();
+    selectedOscillatorType();
+    selectedOscillatorGain();
+    selectedOscillatorOffset();
+};
+
+var oscillatorAnalyser = audioCtx.createAnalyser();
+oscillatorGain.connect(oscillatorAnalyser);
+oscillatorAnalyser.fftSize = 32768;
+var bufferLength = oscillatorAnalyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+
+const w = canvasEl.width;
+const h = canvasEl.height;
+var drawWaveForm = function () {
+    window.requestAnimationFrame(drawWaveForm);
+    oscillatorAnalyser.getByteTimeDomainData(dataArray);
+    canvasCtx.clearRect(0, 0, w, h);
+
+    var sliceWidth = w / bufferLength;
+    var x = 0;
+
+    canvasCtx.beginPath();
+    for (var i = 0; i < bufferLength; i++) {
+        var v = dataArray[i] / 128;
+        var y = h - v * h / 2;
+
+        if(i === 0) {
+            canvasCtx.moveTo(x, y);
+        } else {
+            canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+
+    canvasCtx.stroke();
+
+//    analyser.getByteTimeDomainData(dataArray); // TODO: only when running
+//    canvasCtx.clearRect(0, 0, canvas.width, canvas.height);
+};
+
+drawWaveForm();
 
 var selectedModulator = function () {
     return document.querySelector("input[name=modulator]:checked").value;
@@ -16,15 +76,23 @@ var updateBaseFreq = function () {
 };
 
 var selectedBaseFreq = function () {
-    return parseFloat(document.querySelector("#base-freq").textContent);
+    var freq =
+        parseFloat(document.querySelector("#base-freq").textContent); // Hz
+    oscillator.frequency.value = freq;
+    return freq;
 };
 
 var selectedOscillatorType = function () {
-    return document.querySelector("input[name=oscillator-type]:checked").value;
+    var type =
+        document.querySelector("input[name=oscillator-type]:checked").value;
+    oscillator.type = type;
+    return type;
 };
 
 var selectedOscillatorGain = function () {
-    return parseFloat(document.querySelector("#oscillator-gain").value);
+    var gain = parseFloat(document.querySelector("#oscillator-gain").value);
+    oscillatorGain.gain.value = gain;
+    return gain;
 };
 
 var selectedOutputGain = function (i) {
@@ -36,7 +104,9 @@ var selectedOutputDelay = function (i) {
 };
 
 var selectedOscillatorOffset = function (i) {
-    return parseFloat(document.querySelector("#oscillator-offset").value);
+    var offset = parseFloat(document.querySelector("#oscillator-offset").value);
+    oscillatorOffset.offset.value = offset;
+    return offset;
 };
 
 var selectedOscillatorDetuningFactor = function () {

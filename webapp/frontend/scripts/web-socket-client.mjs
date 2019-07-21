10b825a3ffbbc4f971faceb7996e42cc9d7c95b7
Felix E. Klee <felix.klee@inka.de>
2019-07-21 15:20:11 +0200

    Reuse HTTP port

--- webapp/frontend/scripts/web-socket-client.mjs
+++ webapp/frontend/scripts/web-socket-client.mjs
@@ -1,6 +1,5 @@
 /*jslint browser: true, maxlen: 80 */
 
-var hostname = window.location.hostname;
-var client = new window.WebSocket("ws://" + hostname + ":8080/");
+var client = new window.WebSocket("ws://" + window.location.host);
 
 export default client;

0fc0c394ddd198558fb5251e1101902f0dbad4fa
Felix E. Klee <felix.klee@inka.de>
2019-12-20 13:25:32 +0800

    Allow simulating always connected node A

--- webapp/pair-is-valid.js
+++ webapp/pair-is-valid.js
@@ -3,5 +3,9 @@
 "use strict";
 
 module.exports = function (pair) {
+    var pairIsPermanentlyConnectedNodeA = "^1A0";
+    if (pairIsPermanentlyConnectedNodeA) {
+        return true;
+    }
     return /^[a-zA-Z\^][1-4][a-zA-Z][1-4]$/.test(pair);
 };

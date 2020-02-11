ea16a0c6217a8e50dd913158827aa653be5d07ec
Felix E. Klee <felix.klee@inka.de>
2020-02-11 13:51:13 +0800

    Double buffer size (to fit all colors)

--- hardware/life_size/nodes/ArduinoFirmware/spiTransfer.h
+++ hardware/life_size/nodes/ArduinoFirmware/spiTransfer.h
@@ -1,9 +1,10 @@
 #pragma once
 
 // With DMA enabled, the ESP32 SPI driver wants to receive multiples of 32 bits:
-constexpr uint8_t spiBufferLength = 4;
+constexpr uint8_t spiBufferLength = 8;
 extern char spiRxBuffer[spiBufferLength];
 extern char spiTxBuffer[spiBufferLength];
 
 void spiTransferSetup();
 void doSpiTransfer();
+void clearSpiTxBuffer();

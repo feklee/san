9957088e966081d8aa441fd33d0a316bbaa52d4e
Felix E. Klee <felix.klee@inka.de>
2020-02-17 19:05:35 +0800

    Rename to WiFi for hopefully more clarity

--- webapp/main.js
+++ webapp/main.js
@@ -7,7 +7,7 @@ var program = require("commander");
 program
     .command("list", "List serial ports")
     .command("connect <port>", "Connect to root node on serial port")
-    .command("rebroadcast", "Expects previous log output on stdin")
+    .command("wifi", "Connect to SAN network via WiFi")
     .command("simulate", "Simulate connection")
-    .command("http", "Construct SAN network using HTTP requests")
+    .command("rebroadcast", "Expects previous log output on stdin")
     .parse(process.argv);

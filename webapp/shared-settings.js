13a60afaaa00d1a6c1d833a41790592f5959b935
Felix E. Klee <felix.klee@inka.de>
2020-02-17 11:50:49 +0800

    Export gw

--- webapp/shared-settings.js
+++ webapp/shared-settings.js
@@ -6,53 +6,56 @@ const fs = require("fs");
 var cli = require("./cli");
 var graphUpdateInterval = 0;
 var connectionExpiryDuration = 0;
-var gw; // WiFi gateway
+var gw = []; // WiFi gateway
+
+var assertPositive = function (setting, value) {
+    if (!(value > 0)) {
+        cli.logError("Invalid " + setting);
+        process.exit(1);
+    }
+};
 
 var extractDefines = function (cData) {
-    var assignmentsInC =
+    var definesInC =
         cData.match(new RegExp(
             "^\\s*#define\\s+" +
                 "(graphUpdateInterval|connectionExpiryDuration)" +
                 ".*",
             "gm"
         ));
-    var assignmentsInJs = assignmentsInC.map(
-        function (assignmentInC) {
-            return assignmentInC
+    var assignmentsInJs = definesInC.map(
+        function (defineInC) {
+            return defineInC
                 .replace(/\/\/.*/g, "")
                 .replace(/#define\s+([\w]+)\s+(.*)/g, "$1 = $2;");
         });
     var jsData = assignmentsInJs.join("\n");
-    return jsData;
+    eval(jsData);
+    assertPositive("graphUpdateInterval", graphUpdateInterval);
+    assertPositive("connectionExpiryDuration", connectionExpiryDuration);
 };
 
 var extractGw = function (cData) {
     var matches = cData.match(new RegExp("\\Wgw[^\\w{]*{([\\d\\s,]+)"));
     var s = matches[1];
-    var gw = s.split(",").map((x) => parseInt(x.trim()));
-    return gw;
+    gw = s.split(",").map((x) => parseInt(x.trim()));
 };
 
 try {
     var cData = fs.readFileSync(
         "../hardware/life_size/nodes/ArduinoFirmware/sharedSettings.h",
         "utf8");
-    eval(extractDefines(cData));
-    gw = extractGw(cData);
+    extractDefines(cData);
+    extractGw(cData);
 } catch (ignore) {
     cli.logError("Cannot load shared settings");
     process.exit(1);
 }
 
 module.exports = {
+    gw: gw,
     graphUpdateInterval: graphUpdateInterval,
     connectionExpiryDuration: connectionExpiryDuration
 };
 
-Object.keys(module.exports).forEach(function (setting) {
-    var value = module.exports[setting];
-    if (!(value > 0)) {
-        cli.logError("Invalid " + setting);
-        process.exit(1);
-    }
-});
+console.error(module.exports);

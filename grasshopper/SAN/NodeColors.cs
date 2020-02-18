8e223d0f81068df553ce35e23e96d9e4da46ea32
Felix E. Klee <felix.klee@inka.de>
2020-02-18 13:17:09 +0800

    Add color inputs

--- grasshopper/SAN/NodeColors.cs
+++ grasshopper/SAN/NodeColors.cs
@@ -16,6 +16,14 @@ namespace SAN
         {
             pManager.AddParameter(new ConnectionParameter(), "Connection", "Con", "Connection to SAN", GH_ParamAccess.item);
             pManager.AddTextParameter("Node ID", "ID", "ID of the node", GH_ParamAccess.item);
+            pManager.AddColourParameter("Color 1", "C1", "Color of LED 1", GH_ParamAccess.item);
+            pManager[2].Optional = true;
+            pManager.AddColourParameter("Color 2", "C2", "Color of LED 2", GH_ParamAccess.item);
+            pManager[3].Optional = true;
+            pManager.AddColourParameter("Color 3", "C3", "Color of LED 3", GH_ParamAccess.item);
+            pManager[4].Optional = true;
+            pManager.AddColourParameter("Color 4", "C4", "Color of LED 4", GH_ParamAccess.item);
+            pManager[5].Optional = true;
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
@@ -23,26 +31,40 @@ namespace SAN
             pManager.AddColourParameter("Colors", "C", "Four colors of node", GH_ParamAccess.list);
         }
 
-        protected override void SolveInstance(IGH_DataAccess DA)
+        private List<GH_Colour> loadColorsFromGraph(GraphMessageData d, int nodeIndex)
         {
-            var con = new ConnectionType();
-            DA.GetData(0, ref con);
-
-            string id = "";
-            DA.GetData(1, ref id);
-
-            var d = con.Value.graphMessageData;
-            if (d == null) { return; }
-
-            var i = con.Value.indexOfNode(id);
-            if (i < 0) { return; }
-
-            var colorsOfNodeToConvert = d.colors[i];
+            var colorsOfNodeToConvert = d.colors[nodeIndex];
             var colorsOfNode = new List<GH_Colour>();
             foreach (var colorToConvert in colorsOfNodeToConvert)
             {
                 var color = Color.FromArgb(colorToConvert[0], colorToConvert[1], colorToConvert[2]);
                 colorsOfNode.Add(new GH_Colour(color));
+            }
+            return colorsOfNode;
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            var con = new ConnectionType();
+            DA.GetData(0, ref con);
+
+            var d = con.Value.graphMessageData;
+            if (d == null) { return; }
+
+            string id = "";
+            DA.GetData(1, ref id);
+            var nodeIndex = con.Value.indexOfNode(id);
+            if (nodeIndex < 0) { return; }
+            var colorsOfNode = loadColorsFromGraph(d, nodeIndex);
+            if (colorsOfNode == null) { return; }
+
+            for (int i = 0; i < 4; i++)
+            {
+                var color = new GH_Colour();
+                if (DA.GetData(2 + i, ref color))
+                {
+                    colorsOfNode[i] = color;
+                }
             }
 
             DA.SetDataList(0, colorsOfNode);

8b874c6f12addb4a1e94d7e8bd68772b1ba7ae87
Felix E. Klee <felix.klee@inka.de>
2020-02-20 11:22:15 +0800

    Only send colors to node if connected by WiFi

---
+++ webapp/physical-connection.js
@@ -0,0 +1,11 @@
+// Connection to the physical SAN components
+
+/*jslint node: true, maxlen: 80 */
+
+"use strict";
+
+var physicalConnection = {
+    type: "unknown"
+};
+
+module.exports = physicalConnection;
